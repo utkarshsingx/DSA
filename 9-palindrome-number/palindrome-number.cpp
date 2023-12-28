3d6f6f07f10fd2fd603696debdd10e57b36caf80
Utkarsh <92610758+utkarshsingx@users.noreply.github.com>
2023-12-29 02:50:16 +0530

    Time: 21 ms (6.13%) | Memory: 6.2 MB (98.92%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,27 +1,52 @@
+// Better Solution
+
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int m = x;
-        int ans =0;
-
-        while (m!=0){
-            int digit = m % 10;
-
-             if((ans > INT_MAX/10) || (ans< INT_MIN/10) ){
-                return false;
-            }
-
-            ans = (ans *10) + digit;
-            m = m/10;
-
-        }
-
-        if (( ans == x ) && (ans >= 0)){
-            return true;
-        }
-
-        else{
+        if (x < 0) {
             return false;
         }
+
+        long long reversed = 0;
+        long long temp = x;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            reversed = reversed * 10 + digit;
+            temp /= 10;
+        }
+
+        return (reversed == x);
     }
 };
+
+
+// Brute Force
+
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+//         int m = x;
+//         int ans =0;
+
+//         while (m!=0){
+//             int digit = m % 10;
+
+//              if((ans > INT_MAX/10) || (ans< INT_MIN/10) ){
+//                 return false;
+//             }
+
+//             ans = (ans *10) + digit;
+//             m = m/10;
+
+//         }
+
+//         if (( ans == x ) && (ans >= 0)){
+//             return true;
+//         }
+
+//         else{
+//             return false;
+//         }
+//     }
+// };

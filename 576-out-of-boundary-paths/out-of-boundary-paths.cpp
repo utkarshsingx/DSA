aee0892b4dbe944991e99e1478ca615af7a71e55
Utkarsh <92610758+utkarshsingx@users.noreply.github.com>
2024-01-26 07:57:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (59.44%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/out-of-boundary-paths.cpp
@@ -1,32 +1,33 @@
-
 class Solution {
 public:
-    int findPaths(int m, int n, int N, int x, int y) {
-        const int M = 1000000000 + 7;
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        dp[x][y] = 1;
-        int count = 0;
 
-        for (int moves = 1; moves <= N; moves++) {
-            vector<vector<int>> temp(m, vector<int>(n, 0));
+int dp[51][51][51];
 
-            for (int i = 0; i < m; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (i == m - 1) count = (count + dp[i][j]) % M;
-                    if (j == n - 1) count = (count + dp[i][j]) % M;
-                    if (i == 0) count = (count + dp[i][j]) % M;
-                    if (j == 0) count = (count + dp[i][j]) % M;
-                    temp[i][j] = (
-                        ((i > 0 ? dp[i - 1][j] : 0) + (i < m - 1 ? dp[i + 1][j] : 0)) % M +
-                        ((j > 0 ? dp[i][j - 1] : 0) + (j < n - 1 ? dp[i][j + 1] : 0)) % M
-                    ) % M;
-                }
-            }
-            dp = temp;
-        }
+long long mod = 1e9+7;
 
-        return count;
+int dfs(int i, int j, int n, int m, int moves)
+{
+    if(i<0 || i>=n || j<0 || j>=m)
+    {
+        return 1;
+    }
+    if(moves <= 0)return 0;
+
+    if(dp[i][j][moves] != -1)return dp[i][j][moves];
+
+    int up = dfs(i-1 , j , n , m , moves-1);
+    int down = dfs(i+1 , j , n , m , moves-1);
+    int left = dfs(i , j-1 , n , m , moves-1);
+    int right = dfs(i , j+1 , n , m , moves-1);
+
+    return dp[i][j][moves] = (up%mod + down%mod + left%mod + right%mod)%mod;
+
+}
+
+    int findPaths(int n, int m, int maxMove, int startRow, int startColumn) {
+        
+        memset(dp , -1 , sizeof(dp));
+
+        return dfs(startRow , startColumn , n , m , maxMove);
     }
 };
-
-

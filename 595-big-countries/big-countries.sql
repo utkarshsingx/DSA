6933546f158c3dcfe3dd07c0208555af8cf7e3aa
Utkarsh Singh <92610758+utkarshsingx@users.noreply.github.com>
2025-03-01 14:04:25 +0530

    Time: 363 ms (86.32%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+# take care of AND/ OR notations, here asked if any one condition meets than country is big so OR is used, 1 attempt i selected AND that was my mistake
 
 SELECT name, population, area FROM World
 WHERE area >= 3000000 OR population >= 25000000;
